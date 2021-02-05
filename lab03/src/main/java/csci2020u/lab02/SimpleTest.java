a2bb61bfd990b6fa14754236abc8de9844775f7a
nalam2000 <nalam8493@gmail.com>
2021-02-04 19:45:44 -0500

    did lab03

---
+++ lab03/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,10 @@
+package csci2020u.lab02;
+
+public class SimpleTest {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, I'm Noor");
+    }
+
+}
